917cd73de0ac643762166554fddfb794636ab424
jaewookyou <jaewook376@naver.com>
2020-08-11 15:24:21 +0900

    pip support

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name                = 'arang',
-    version             = '0.1',
+    version             = '0.4',
     description         = 'my own module for webhacking using python3',
     long_description	= long_description,
     long_description_content_type	= "text/markdown",
---
+++ test/test.sh
@@ -0,0 +1,3 @@
+python3 -m pip install arang;
+python3 -m pip install -U arang;
+python3 test.py
